0f7f5f6187d8969c1c9417fcb47918ff61216a48
shrey <79036238+shreyzeous21@users.noreply.github.com>
2023-05-01 20:55:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Editor from "./components/Editor";
+import useLocalStorage from "./hooks/useLocalStorage";
+function App() {
+  const [html, setHtml] = useLocalStorage("html", "");
+  const [css, setCss] = useLocalStorage("css", "");
+  const [js, setJs] = useLocalStorage("js", "");
+  const [srcDoc, setSrcDoc] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+            <html>
+              <body>${html}</body>
+              <style>${css}</style>
+              <script>${js}</script>
+            </html>`);
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
+  return (
+    <div className="app">
+      <div className="app__header">
+        <div className="app__logo">
+          <img src="https://i.imgur.com/vGVAieX.png" alt="logo" />
+          <h3>
+            Code
+            <br />
+            Fragment
+          </h3>
+        </div>
+      </div>
+      <div className="app__pane app__topPane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="Javascript"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="app__outputPane">
+        <div className="app__outputTitle">OUTPUT</div>
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          frameBorder="0"
+          sandbox="allow-scripts"
+          width="100%"
+          height="100%"
+          className="app__iframe"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Editor.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
+
+function Editor({ language, displayName, value, onChange }) {
+  const [open, setOpen] = useState(true);
+  return (
+    <div className={`editor ${open ? "" : "app__collapsed"}`}>
+      <div className="editor__title">
+        {displayName}
+        <button
+          className="editor__expandCollapseButton"
+          onClick={() => setOpen((prevOpen) => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={(editor, data, value) => {
+          onChange(value);
+        }}
+        value={value}
+        className="editor__codeMirrorWrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          lineNumbers: true,
+          theme: "material",
+        }}
+      />
+    </div>
+  );
+}
+
+export default Editor;
